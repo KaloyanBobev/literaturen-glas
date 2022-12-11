b8271baf366beaaab25ab479c590a4df3da7b451
Kaloyan <kaloyanbobev@gmail.com>
2022-12-11 21:18:22 +0000

    Update Book.js

--- src/components/Books/Book.js
+++ src/components/Books/Book.js
@@ -1,39 +1,34 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { useTranslation } from "react-i18next";
 
 function Book({ poems, tracked }) {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
+  return (
+    <div>
+      <article className="row">
+        <h3 className="col-md-3">{t("booksTrans.author")}</h3>
+        <h3 className="col-md-3">{t("booksTrans.name")}</h3>
+        <h3 className="col-md-3">{t("booksTrans.style")}</h3>
+        <h3 className="col-md-3">{t("booksTrans.issueDate")}</h3>
+      </article>
 
-
-    return (
-        <div>
-            <article className="row">
-                <h3 className="col-md-3">{t('booksTrans.author')}</h3>
-                <h3 className="col-md-3">{t('booksTrans.name')}</h3>
-                <h3 className="col-md-3">{t('booksTrans.style')}</h3>
-                <h3 className="col-md-3">{t('booksTrans.issueDate')}</h3>
+      {poems
+        .filter((name) =>
+          name.author.toLocaleLowerCase().includes(tracked.toLocaleLowerCase())
+        )
+        .map((book) => {
+          return (
+            <article key={book.id} className="row">
+              <div className="col-md-3 ">{book.author}</div>
+              <div className="col-md-3">{book.name}</div>
+              <div className="col-md-3">{book.species}</div>
+              <div className="col-md-3">{book.date}</div>
             </article>
-
-            {
-                poems.filter((name) =>
-                    name.author
-                        .toLocaleLowerCase()
-                        .includes(tracked.toLocaleLowerCase()))
-                    .map((book) => {
-                        return (
-                            <article key={book.id} className="row">
-                                <div className="col-md-3 ">{book.author}</div>
-                                <div className="col-md-3">{book.name}</div>
-                                <div className="col-md-3">{book.species}</div>
-                                <div className="col-md-3">{book.date}</div>
-                            </article>
-                        )
-                    })
-            }
-        </div>
-    )
+          );
+        })}
+    </div>
+  );
 }
-
 
 export default Book;
