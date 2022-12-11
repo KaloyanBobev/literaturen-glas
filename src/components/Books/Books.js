76496b7d45a9e08dac16aa21098076d21e2bf1eb
Kaloyan <kaloyanbobev@gmail.com>
2022-12-11 21:18:16 +0000

    add useState and useEffect

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,66 +1,66 @@
-import React, { Suspense } from 'react';
-import books from '../../data/books.json';
-import './Books.scss';
-import Book from './Book';
-import { withTranslation } from 'react-i18next';
-
+import React, { useState, UseEffect, Suspense, useEffect } from "react";
+import books from "../../data/books.json";
+import "./Books.scss";
+import Book from "./Book";
+import { withTranslation } from "react-i18next";
 
 class LegacyComponentClass extends React.Component {
+  render() {
+    const { t } = this.props;
 
-    render() {
-        const { t } = this.props;
-
-        return (
-            <>
-                <h1 className="center">{t('booksTrans.header')}</h1>
-                <label>
-                    {t('booksTrans.sort')}
-                </label>
-
-            </>
-        )
-    }
+    return (
+      <>
+        <h1 className="center">{t("booksTrans.header")}</h1>
+        <label>{t("booksTrans.sort")}</label>
+      </>
+    );
+  }
 }
 
-const MyComponent = withTranslation()(LegacyComponentClass)
+const MyComponent = withTranslation()(LegacyComponentClass);
 
 class Books extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            isReverse: false,
-            tracked: "",
-            poems: []
-        }
-        this.filterHadler = this.filterHadler.bind(this);
-    }
+  // const [isReverse, setIsReverse] = useState(false);
+  // const [tracked, setTracked] = useState("");
+  // const [poems, setPoems] = useState([]);
+  constructor() {
+    super();
+    this.state = {
+      isReverse: false,
+      tracked: "",
+      poems: [],
+    };
+    this.filterHadler = this.filterHadler.bind(this);
+  }
 
-    componentDidMount() {
-        this.setState({
-            poems: books,
-        })
-    }
+  componentDidMount() {
+    this.setState({
+      poems: books,
+    });
+  }
+  // useEffect(() => {
+  //     setPoems(books);
+  // });
+  filterHadler(e) {
+    this.setState({
+      tracked: e.target.value,
+    });
+  }
 
-    filterHadler(e) {
-        this.setState({
-            tracked: e.target.value
-        })
-    }
-
-    render() {
-        return (
-            <Suspense fallback="loading">
-                <section id="books">
-                    <MyComponent />
-                    <input
-                        placeholder=""
-                        value={this.state.tracked}
-                        onChange={this.filterHadler}
-                    />
-                    <Book poems={this.state.poems} tracked={this.state.tracked} />
-                </section>
-            </Suspense>
-        )
-    }
+  render() {
+    return (
+      <Suspense fallback="loading">
+        <section id="books">
+          <MyComponent />
+          <input
+            placeholder=""
+            value={this.state.tracked}
+            onChange={this.filterHadler}
+          />
+          <Book poems={this.state.poems} tracked={this.state.tracked} />
+        </section>
+      </Suspense>
+    );
+  }
 }
 export default Books;
